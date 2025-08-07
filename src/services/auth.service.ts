06870d83d83ec327951c88587fa5cf9c3d723608
Aleksander Baska <baskaaleksander03@gmail.com>
2025-08-07 12:08:36 +0200

    feat: add login functionality with validation and error handling

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -27,3 +27,29 @@ export const registerUser = async (data: UserAuthenticationSchema) => {
 
   return userWithoutPassword;
 };
+
+export const loginUser = async (data: UserAuthenticationSchema) => {
+  const user = await prismaClient.user.findUnique({
+    where: { email: data.email },
+  });
+
+  if (!user) {
+    const err = new Error('User with that email does not exist') as AppError;
+    err.statusCode = 404;
+    throw err;
+  }
+
+  const isValidPassword = bcrypt.compareSync(data.password, user.passwordHash);
+
+  console.log(isValidPassword);
+
+  if (!isValidPassword) {
+    const err = new Error('Incorrect password') as AppError;
+    err.statusCode = 409;
+    throw err;
+  }
+
+  const { passwordHash, ...userWithoutPassword } = user;
+
+  return userWithoutPassword;
+};
