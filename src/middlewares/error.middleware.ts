7284699c86accd5d08b79f44dc30bf3de891b107
Aleksander Baska <baskaaleksander03@gmail.com>
2025-08-07 13:08:06 +0200

    feat: add helmet middleware for enhanced security and update error middleware signature

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,7 +1,16 @@
 import { AppError } from '../utils/types';
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
-export const errorMiddleware = (err: AppError, req: Request, res: Response) => {
+export const errorMiddleware = (
+  err: AppError,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
 
