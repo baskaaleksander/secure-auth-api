75669f3e99ad4fcc4b6f132986b7f36f54e89177
Aleksander Baska <baskaaleksander03@gmail.com>
2025-08-07 09:58:19 +0200

    fix: security vulnerabilities

--- src/config/env.ts
+++ src/config/env.ts
@@ -9,9 +9,16 @@ interface Config {
   nodeEnv: string;
 }
 
+if (!process.env.JWT_SECRET) {
+  throw new Error('JWT_SECRET is required! Define it in .env file');
+}
+if (!process.env.JWT_REFRESH_SECRET) {
+  throw new Error('JWT_REFRESH_SECRET is required! Define it in .env file');
+}
+
 const config: Config = {
-  jwtSecret: process.env.JWT_SECRET || 'yoursecuretoken',
-  jwtRefreshSecret: process.env.JWT_SECRET || 'yoursecuretoken',
+  jwtSecret: process.env.JWT_SECRET,
+  jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
   port: Number(process.env.PORT) || 3000,
   nodeEnv: process.env.NODE_ENV || 'development',
 };
